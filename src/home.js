d8b62cb0b8caa466d9358d82284ec0412745f0a7
Gasper Katrasnik <gkatrasnik@gmail.com>
2020-12-22 22:55:02 +0100

    home page done

--- src/home.js
+++ src/home.js
@@ -1,3 +1,4 @@
+import menu from "./menu";
 
 
 function home() {
@@ -7,6 +8,23 @@ function home() {
     const title = document.createElement("h1");
     title.textContent = "Welcome to our Restaurant"
     tabContent.appendChild(title);
+
+
+    const buttonContainer = document.createElement("div")
+    buttonContainer.setAttribute("class", "button-container")
+    
+
+    const button = document.createElement("button");
+    button.textContent="Menu";
+    button.setAttribute("class", "menu-button")
+
+    buttonContainer.appendChild(button);
+    tabContent.appendChild(buttonContainer);
+
+    button.addEventListener("click", () => {
+        menu();
+    })
+
 }
 
 export default home;
